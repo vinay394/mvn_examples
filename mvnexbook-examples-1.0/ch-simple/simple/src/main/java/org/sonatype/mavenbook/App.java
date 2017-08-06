2b99fdffa3ec04959319eecc4b6ad604e56cc073
Vinay <vinaykumar.394@gmail.com>
2017-08-05 22:02:00 -0700

    first push

---
+++ mvnexbook-examples-1.0/ch-simple/simple/src/main/java/org/sonatype/mavenbook/App.java
@@ -0,0 +1,13 @@
+package org.sonatype.mavenbook;
+//import org.zkoss.calendar.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jan Batch. Welcome to Formac !!!" );
+		}
+}
